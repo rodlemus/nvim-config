a053b5b54eadf70dfde75ebbc02bf538aca2d8c4
Rodrigo Lemus <rodlemus30@gmail.com>
2023-04-19 09:32:38 -0600

    feat: lsp config added

--- lua/user/main.lua
+++ lua/user/main.lua
@@ -40,32 +40,35 @@ lazy.setup({
 
   },
   { "ellisonleao/gruvbox.nvim", priority = 1000 },
-{
-  "nvim-telescope/telescope.nvim",
-  dependencies = {
-    { "nvim-lua/plenary.nvim" },
-    { "nvim-lua/popup.nvim" },
-    { "nvim-telescope/telescope-fzy-native.nvim" },
-    { "kyazdani42/nvim-web-devicons" },
-    { "nvim-telescope/telescope-file-browser.nvim" },
-    { "nvim-telescope/telescope-ui-select.nvim" },
-    {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      dependencies = {
-        "junegunn/fzf.vim",
-        dependencies = {
-          {
-            "tpope/vim-dispatch",
-            cmd = { "Make", "Dispatch" },
-          },
-          {
-            "junegunn/fzf",
-            build = ":call fzf#install()",
-          }
-        },
-      },
-    },
-  },
-}})
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-lua/popup.nvim" },
+			{ "nvim-telescope/telescope-fzy-native.nvim" },
+			{ "kyazdani42/nvim-web-devicons" },
+			{ "nvim-telescope/telescope-file-browser.nvim" },
+			{ "nvim-telescope/telescope-ui-select.nvim" },
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+				dependencies = {
+					"junegunn/fzf.vim",
+					dependencies = {
+						{
+							"tpope/vim-dispatch",
+							cmd = { "Make", "Dispatch" },
+						},
+						{
+							"junegunn/fzf",
+							build = ":call fzf#install()",
+						}
+					},
+				},
+			},
+		},
+	},
+	{'xiyaowong/transparent.nvim'},
+  {"neovim/nvim-lspconfig"},
+})
 
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -51,3 +51,14 @@ require('gruvbox').setup({
         enable = true,
       },
  })
+require("transparent").setup({
+  groups = { -- table: default groups
+    'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+    'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+    'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+    'SignColumn', 'CursorLineNr', 'EndOfBuffer',
+  },
+  extra_groups = {}, -- table: additional groups that should be cleared
+  exclude_groups = {}, -- table: groups you don't want to clear
+})
+require("pluginsconfig.lsp")
